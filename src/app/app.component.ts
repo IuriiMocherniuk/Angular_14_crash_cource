b38411939d6f00c51dec31e0da65f10d04533c2a
Iurii Mocherniuk <iurii.mocherniuk@gmail.com>
2022-08-25 22:03:58 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {IProduct} from "./modules/product";
+import {ProductsService} from "./services/products.service";
+import {products} from "./data/products";
+import {Observable, tap} from "rxjs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-crash-course-2022';
+export class AppComponent implements OnInit{
+  title = 'angular app'
+  // products: IProduct[] = []
+  loading: boolean = false
+  products$: Observable<IProduct[]>
+  term = ''
+
+
+  constructor(private productService: ProductsService) {
+    }
+
+  ngOnInit(): void {
+    this.loading = true
+    this.products$ = this.productService.getAll().pipe(
+      tap(()=> this.loading = false)
+    )
+    // this.productService.getAll().subscribe(products => {
+    //   this.products = products
+    //   this.loading = false
+    // })
+  }
+
+
 }
